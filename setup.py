617376c2b858c9659f1edb44c437dc6bb18753d0
G. Bogans <mauludsadiq@gmail.com>
2025-10-05 08:15:59 +0200

    Move metadata to setup.py to fix missing Name/Version; v0.2.6

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
+    name="onestepx",
+    version="0.2.6",
+    description="Instant hierarchical collapse to terminal node (O(1) traversal)",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Maulud Sadiq",
+    author_email="mauludsadiq@gmail.com",
+    license="MIT",
+    url="https://github.com/mauludsadiq/onestep",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
