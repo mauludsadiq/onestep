22e13ba7266dcb8a0129acf094d297d1b8e31d92
G. Bogans <mauludsadiq@gmail.com>
2025-10-05 08:37:41 +0200

    Remove setup.cfg; embed metadata in setup.py; build 0.3.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 setup(
     name="onestepx",
-    version="0.3.1",
+    version="0.3.4",
     description="Instant hierarchical collapse to terminal node (O(1) traversal)",
     long_description=Path("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
